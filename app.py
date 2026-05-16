8de8ccdd070000336c9d3a616a17c8f6a898ac2a
lubrificacaomaquinassv-cloud <lubrificacaomaquinassv@gmail.com>
2026-05-16 20:32:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ with col_logo:
     st.image("https://i.postimg.cc/Y9X7ddnb/LOGO-BP.jpg", width=110)
 with col_titulo:
     st.title("🚜 Gestão de Oficina - SV")
-    st.caption(🛠️📈"SIGCF — Sistema Integrado de Gestão de Custos de Frota")
+    st.caption("SIGCF — Sistema Integrado de Gestão de Custos de Frota")
 
 st.divider()
 
